c6b35cc278e0c4d3d58f9a839c9c70dcd9f0e384
gabigol_96@icloud.com <gabigol_96@icloud.com>
2023-08-29 17:31:22 +0100

    finished the history page

--- src/pages/History/History.tsx
+++ src/pages/History/History.tsx
@@ -1,7 +1,71 @@
+import { HistoryContainer, HistoryList } from './styles'
+
 export function History() {
   return (
-    <div>
-      <h1>History page</h1>
-    </div>
+    <HistoryContainer>
+      <h1>Meu Histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Conserto no Checkout Builder</td>
+              <td>25 minutos</td>
+              <td>Há Cerca de 2 messes</td>
+              <td>Em Andamento</td>
+            </tr>
+            <tr>
+              <td>Conserto no Checkout Builder</td>
+              <td>25 minutos</td>
+              <td>Há Cerca de 2 messes</td>
+              <td>Em Andamento</td>
+            </tr>
+            <tr>
+              <td>Conserto no Checkout Builder</td>
+              <td>25 minutos</td>
+              <td>Há Cerca de 2 messes</td>
+              <td>Em Andamento</td>
+            </tr>
+            <tr>
+              <td>Conserto no Checkout Builder</td>
+              <td>25 minutos</td>
+              <td>Há Cerca de 2 messes</td>
+              <td>Em Andamento</td>
+            </tr>
+            <tr>
+              <td>Conserto no Checkout Builder</td>
+              <td>25 minutos</td>
+              <td>Há Cerca de 2 messes</td>
+              <td>Em Andamento</td>
+            </tr>
+            <tr>
+              <td>Conserto no Checkout Builder</td>
+              <td>25 minutos</td>
+              <td>Há Cerca de 2 messes</td>
+              <td>Em Andamento</td>
+            </tr>
+            <tr>
+              <td>Conserto no Checkout Builder</td>
+              <td>25 minutos</td>
+              <td>Há Cerca de 2 messes</td>
+              <td>Em Andamento</td>
+            </tr>
+            <tr>
+              <td>Conserto no Checkout Builder</td>
+              <td>25 minutos</td>
+              <td>Há Cerca de 2 messes</td>
+              <td>Em Andamento</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
